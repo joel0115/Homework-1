d8e2c6477705854468676a034eca3b96f7c7c0f3
joel0115 <joel0115@gmail.com>
2024-03-16 21:03:56 +0800

    Classroom completed

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    bool registered = false;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(registered == false){
+            registered = true;
+            return 1000;
+        }
+        else{
+            return 123;
+        }
     }
 }
 
@@ -18,6 +25,13 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        bool isEnrolled = IClassroomV2(address(msg.sender)).isEnrolled();
+        if(!isEnrolled){
+            return 1000;
+        }
+        else{
+            return 123;
+        }
     }
 }
 
@@ -25,5 +39,11 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(gasleft() >= 7000){
+            return 1000;
+        }
+        else{
+            return 123;
+        }
     }
 }
