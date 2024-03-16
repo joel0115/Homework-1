ea5a1794b40d5df5b297461a8eb67636b9eee1be
joel0115 <joel0115@gmail.com>
2024-03-16 19:45:22 +0800

    LiaoToken completed

--- hw1/src/LiaoToken/LiaoToken.sol
+++ hw1/src/LiaoToken/LiaoToken.sol
@@ -17,6 +17,7 @@ contract LiaoToken is IERC20 {
     // TODO: you might need to declare several state variable here
     mapping(address account => uint256) private _balances;
     mapping(address account => bool) isClaim;
+    mapping(address owner => mapping (address spender => uint256)) _allowance;
 
     uint256 private _totalSupply;
 
@@ -60,17 +61,41 @@ contract LiaoToken is IERC20 {
 
     function transfer(address to, uint256 amount) external returns (bool) {
         // TODO: please add your implementaiton here
+        if(_balances[msg.sender] < amount){
+            revert();
+        }
+        
+        emit Transfer(msg.sender, to, amount);
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+        return true;
     }
 
     function transferFrom(address from, address to, uint256 value) external returns (bool) {
         // TODO: please add your implementaiton here
+        if(_allowance[from][msg.sender] < value){
+            revert();
+        }
+        if(_balances[from] < value){
+            revert();
+        }
+        emit Transfer(from, to, value);
+        _allowance[from][msg.sender] -= value;
+        _balances[from] -= value;
+        _balances[to] += value;
+        return true;
+
     }
 
     function approve(address spender, uint256 amount) external returns (bool) {
         // TODO: please add your implementaiton here
+        emit Approval(msg.sender, spender, amount);
+        _allowance[msg.sender][spender] = amount;
+        return true;
     }
 
     function allowance(address owner, address spender) public view returns (uint256) {
         // TODO: please add your implementaiton here
+        return _allowance[owner][spender];
     }
 }
